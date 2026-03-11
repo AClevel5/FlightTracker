efdcc8bb53dc874ebb54c9652ccee02dddca1091
Alex Cleveland <alexcleveland5@gmail.com>
2026-03-10 18:48:27 -0600

    Get flight data

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,19 @@
+import requests
+SHEET_URL = "https://api.sheety.co/14f7d1cbe8332fa058d95bac9f80e54f/flightTracker/flights"
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        pass
+
+    def update_flight_cost(self, id):
+        response = requests.post(f"{SHEET_URL}/{id}")
+        return response.json()["Lowest Price"]
+
+    def get_existing_flight_data(self):
+        response = requests.get(SHEET_URL)
+        current_flight_data = response.json()
+        return current_flight_data
+
+
+
--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import requests
+from data_manager import DataManager
+
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+
+data_manager = DataManager()
+flight_data = data_manager.get_existing_flight_data()
